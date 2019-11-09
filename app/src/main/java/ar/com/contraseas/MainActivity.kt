c08118bec5fa854b9b1cee660963611ccc61f312
Joaquin Garcia <jokogarcia@gmail.com>
2019-11-09 11:05:37 -0300

    Clase 3 Spinner agregado

--- app/src/main/java/ar/com/contraseas/MainActivity.kt
+++ app/src/main/java/ar/com/contraseas/MainActivity.kt
@@ -1,16 +1,54 @@
 package ar.com.contraseas
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val claves = mutableMapOf<String,String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val prefs = getSharedPreferences("MisClaves", Context.MODE_PRIVATE)
+        val copy = prefs.all
+        for ((nombre,clave) in copy ){
+            claves.put(nombre, clave as String)
+        }
+
+        val array = ArrayList(claves.keys)
+        val adapter = ArrayAdapter(this,R.layout.spinner_elemento_visible,
+            array )
+        adapter.setDropDownViewResource(R.layout.spinner_elemento_visible)
+        spNombre.adapter = adapter
+        spNombre.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onItemSelected(
+                    spinner: AdapterView<*>?,
+                    vista: View?,
+                    indice: Int,
+                    id: Long) {
+
+                    val nombre = spinner?.adapter?.getItem(indice)
+                    tvClave.text = claves[nombre]
+
+
+                }
+
+            }
+
+
+
     }
 
     fun onClickGenerar(view: View) {
@@ -26,8 +64,10 @@ class MainActivity : AppCompatActivity() {
         val nombre = etNombreClave.text.toString()
         val clave = tvClave.text.toString()
         claves.put(nombre,clave)
-
-
+        val prefs = getSharedPreferences("MisClaves", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString(nombre,clave)
+        editor.commit()
     }
 
     fun onClickVerClave(view: View) {
